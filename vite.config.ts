ab7fdc142b7d5684a88d45c1161c9cf2aaf69d3e
Nathan Ag <91394955+NA-Ag@users.noreply.github.com>
2025-12-28 14:51:57 -0600

    Change base URL in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
-      base: '/penko-software-hub/',
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -17,3 +17,4 @@ export default defineConfig(() => {
       }
     };
 });
+
